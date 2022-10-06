f28a1770e69d4a1b7f8a9d640782749f5ec4d979
KKrajewski2 <kkrajew2@stu.vistula.edu.pl>
2022-10-06 16:17:48 +0200

    Commit 4

--- src/pl/vistula/helloworldkrajewski/HelloWorldKrajewski.java
+++ src/pl/vistula/helloworldkrajewski/HelloWorldKrajewski.java
@@ -5,7 +5,7 @@ public class HelloWorldKrajewski {
 
         System.out.println("Hello Vistula Krajewski");
 
-        System.out.println("Hello Vistula Krajewski1");
+
 
         System.out.println("Hello Vistula Krajewski2");
 
